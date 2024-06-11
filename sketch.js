9a8f4957a15a2fd1a79fef8079635e9a9356dff9
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-06-10 21:30:00 -0700

    added buttons and colors

--- sketch.js
+++ sketch.js
@@ -1,9 +1,31 @@
 let time = 0;
 let wave = [];
 let x_shift = 300;
+let n;
+let input, button;
+let m = 10;
+let pressed = false;
+let lineColor = [50, 200, 220];
+let bgColor = [0, 30, 30];
 
 function setup() {
   createCanvas(1440, 900);
+
+  let buttonSawtooth = createButton("switch wave");
+  buttonSawtooth.position(700, 700);
+  buttonSawtooth.size(100, 50);
+  buttonSawtooth.mouseClicked(() => {
+    pressed = !pressed;
+  });
+
+  input = createInput("10");
+  input.position(400, 700);
+
+  button = createButton('submit');
+  button.position(input.x + input.width + 5, input.y);
+  button.mousePressed(() => {
+    m = input.value();
+  });
 }
 
 let centre_x = (1440 / 2) - 400;
@@ -12,7 +34,8 @@ let centre_y = 900 / 2;
 let r_ = 120;
 
 function draw() {
-  background(0,30,30);
+  bgColor = pressed ? [30, 5, 0] : [0, 30, 30];
+  background(bgColor);
 
   stroke(255);
   noFill();
@@ -21,14 +44,14 @@ function draw() {
   let x = centre_x;
   let y = centre_y;
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < m; i++) {
 
     let px = x;
     let py = y;
 
-    let n = 2 * i + 2;
+    n = pressed ? 2 * i + 2 : 2 * i + 1;
 
-    let r = r_ * (-1)**(i+1) * (2 / (n * PI));
+    let r = r_ * (2 / (n * PI));
 
     x += r * cos(n * time);
     y += r * sin(n * time);
@@ -44,7 +67,7 @@ function draw() {
 
   }
 
-  strokeWeight(4);
+  strokeWeight(2);
 
   // horizontal line from last point
   line(x, y, centre_x + x_shift, wave[0]);
@@ -57,7 +80,7 @@ function draw() {
   line(centre_x + x_shift, centre_y, centre_x + x_shift + 640, centre_y);
 
   // gridlines
-  strokeWeight(4);
+  strokeWeight(2);
   stroke(255, 50);
   for (let i = -3; i < 4; i++) {
     line(centre_x + x_shift, centre_y + i*60, centre_x + x_shift + 640, centre_y + i*60);
@@ -68,9 +91,11 @@ function draw() {
 
   wave.unshift(y);
 
-  fill(50, 200, 220);
-  stroke(50, 200, 220);
-  strokeWeight(5);
+  lineColor = pressed ? [220, 80, 40] : [50, 200, 220];
+
+  fill(lineColor);
+  stroke(lineColor);
+  strokeWeight(3);
   circle(centre_x + x_shift, y, r_/10, wave[0]);
 
   beginShape();
@@ -86,4 +111,9 @@ function draw() {
   if (wave.length > 600) {
     wave.pop();
   }
+}
+
+function switchToSawtooth() {
+  console.log("clicked");
+  n = 2*i + 2; 
 }
